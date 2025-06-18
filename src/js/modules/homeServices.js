8225cbfc4e2952ac07c12d45a4d591c07f88f498
andre.botelho <andre.botelho@emc.com.br>
2025-06-18 10:47:20 -0300

    Ajusta index para ficar com 6 cards

--- src/js/modules/homeServices.js
+++ src/js/modules/homeServices.js
@@ -46,16 +46,16 @@ export class HomeServicesManager {
         popular: false,
         url: "servicos.html?group=saude",
       },
-      {
-        id: 5,
-        name: "IPTU",
-        description:
-          "Emissão de guias, solicitar isenção, recursos, certidão, dúvidas.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
+      // {
+      //   id: 5,
+      //   name: "IPTU",
+      //   description:
+      //     "Emissão de guias, solicitar isenção, recursos, certidão, dúvidas.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
       {
         id: 6,
         name: "ITBI",
@@ -75,99 +75,99 @@ export class HomeServicesManager {
         iconClass: "icon-blue", // classe CSS para estilização do ícone
         popular: false,
         url: "servicos.html?group=saude",
-      },
-      {
-        id: 8,
-        name: "SIMPLES NACIONAL",
-        description:
-          "Consulta de impedimentos para opção do Simples Nacional em 2025.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 9,
-        name: "DECLARAÇÃO ELETRÔNICA",
-        description: "Declaração Eletrônica de Serviços.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 10,
-        name: "NOTA FISCAL ELETRÔNICA",
-        description: "Emissão de Nota Fiscal Eletrônica.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 11,
-        name: "GUIA DE PREÇO PÚBLICO",
-        description: "Emissão de guia de preços públicos diversos.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 12,
-        name: "DEC",
-        description:
-          "Comunicação por meio eletrônico da Administração Tributária Municipal com os Contribuintes.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 13,
-        name: "CERTIDÕES",
-        description: "Emissão e autenticação de certidões.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 14,
-        name: "VAF",
-        description: "Valor Adicionado Fiscal.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 15,
-        name: "PROTOCOLO",
-        description: "Consulta Tramitação de Protocolo.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 16,
-        name: "CONTAC",
-        description: "Informações sobre o Conselho Administrativo de Contagem",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
-      {
-        id: 17,
-        name: "CÓDIGO TRIBUTÁRIO DO MUNICÍPIO DE CONTAGEM",
-        description: "Download do Código Tributário do Município de Contagem.",
-        icon: "fa fa-calendar", // icone da família de fontes
-        iconClass: "icon-blue", // classe CSS para estilização do ícone
-        popular: false,
-        url: "servicos.html?group=saude",
-      },
+      }
+      // {
+      //   id: 8,
+      //   name: "SIMPLES NACIONAL",
+      //   description:
+      //     "Consulta de impedimentos para opção do Simples Nacional em 2025.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 9,
+      //   name: "DECLARAÇÃO ELETRÔNICA",
+      //   description: "Declaração Eletrônica de Serviços.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 10,
+      //   name: "NOTA FISCAL ELETRÔNICA",
+      //   description: "Emissão de Nota Fiscal Eletrônica.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // }
+      // {
+      //   id: 11,
+      //   name: "GUIA DE PREÇO PÚBLICO",
+      //   description: "Emissão de guia de preços públicos diversos.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 12,
+      //   name: "DEC",
+      //   description:
+      //     "Comunicação por meio eletrônico da Administração Tributária Municipal com os Contribuintes.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 13,
+      //   name: "CERTIDÕES",
+      //   description: "Emissão e autenticação de certidões.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 14,
+      //   name: "VAF",
+      //   description: "Valor Adicionado Fiscal.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 15,
+      //   name: "PROTOCOLO",
+      //   description: "Consulta Tramitação de Protocolo.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 16,
+      //   name: "CONTAC",
+      //   description: "Informações sobre o Conselho Administrativo de Contagem",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
+      // {
+      //   id: 17,
+      //   name: "CÓDIGO TRIBUTÁRIO DO MUNICÍPIO DE CONTAGEM",
+      //   description: "Download do Código Tributário do Município de Contagem.",
+      //   icon: "fa fa-calendar", // icone da família de fontes
+      //   iconClass: "icon-blue", // classe CSS para estilização do ícone
+      //   popular: false,
+      //   url: "servicos.html?group=saude",
+      // },
     ];
   }
 
